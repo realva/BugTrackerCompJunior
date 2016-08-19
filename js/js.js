d04c2548413a18bf344465faa1ffa248fc584261
Highlander-Paiva <highlander-7@hotmail.com>
2016-08-19 19:44:46 -0300

    Layout de característica previsível

--- js/js.js
+++ js/js.js
@@ -1,39 +1,36 @@
-
-
-var btnsRipple = function (e){
-	var target = e.target;
-	if (target.tagName.toLowerCase() !== 'button') return false;
-	var rect = target.getBoundingClientRect();
-	var ripple = target.querySelector('.ripple');
-	if (!ripple){
-		ripple = document.createElement ('span');
-		ripple.className = 'ripple';
-		ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
-		target.appendChild(ripple);
-	}
-	ripple.classList.remove('show');
-	var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
-	var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
-	ripple.style.top = top + 'px';
-	ripple.style.left = left + 'px';
-	ripple.classList.add('show');
-	return false;
+var btnsRipple = function (e) {
+    var target = e.target;
+    if (target.tagName.toLowerCase() !== 'button') return false;
+    var rect = target.getBoundingClientRect();
+    var ripple = target.querySelector('.ripple');
+    if (!ripple) {
+        ripple = document.createElement('span');
+        ripple.className = 'ripple';
+        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px';
+        target.appendChild(ripple);
+    }
+    ripple.classList.remove('show');
+    var top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+    var left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+    ripple.style.top = top + 'px';
+    ripple.style.left = left + 'px';
+    ripple.classList.add('show');
+    return false;
 }
 
 document.addEventListener('click', btnsRipple, false);
 
-$(document).ready(function(){
-	$(".imputtext").blur(function(){
+$(document).ready(function () {
+    $(".imputtext").blur(function () {
 
-		if($(this).val())
-			$(this).addClass('used');
-		else
-			$(this).removeClass('used');
+        if ($(this).val())
+            $(this).addClass('used');
+        else
+            $(this).removeClass('used');
 
-	});
+    });
 
 });
-$( function() {
-    $( "#tabs" ).tabs();
-  } );
-
+$(function () {
+    $("#tabs").tabs();
+});
