c1b219383679e0fb3176afc85187f136fd659647
Bruno Vianna de Oliveira <bviannaoliveira@gmail.com>
2016-08-20 18:37:18 -0300

    Correção Erro de compilação

--- Scripts-BugTracker/popularBDBugTracker.sql
+++ Scripts-BugTracker/popularBDBugTracker.sql
@@ -12,16 +12,16 @@ INSERT INTO projeto (idProjeto, nome, plataforma, tester, descricao) VALUES ('4'
 INSERT INTO projeto (idProjeto, nome, plataforma, tester, descricao) VALUES ('5', 'Ponto Eletrônico Comp', 'Android', 'Matheus Nogueira', 'PS2016');
 
 
-INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('1', 'NomeDoErro1', 'Erro de compilcação', 'PS2016', '1');
+INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('1', 'NomeDoErro1', 'Erro de compilação', 'PS2016', '1');
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('2', 'NomeDoErro2', 'Interface', 'PS2016', '1'); 
-INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('3', 'NomeDoErro3', 'Erro de compilcação', 'PS2016','3'); 
+INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('3', 'NomeDoErro3', 'Erro de compilação', 'PS2016','3'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('4', 'NomeDoErro4', 'Erro de lógica', 'PS2016', '1'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('5', 'NomeDoErro5', 'Interface', 'PS2016', '2'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('6', 'NomeDoErro6', 'Erro de execução', 'PS2016', '1'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('7', 'NomeDoErro7', 'Erro de lógica', 'PS2016', '1'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('8', 'NomeDoErro8', 'Interface', 'PS2016', '1'); 
-INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('9', 'NomeDoErro9', 'Erro de compilcação', 'PS2016', '2'); 
-INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('10', 'NomeDoErro10', 'Erro de compilcação', 'PS2016', '3'); 
+INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('9', 'NomeDoErro9', 'Erro de compilação', 'PS2016', '2'); 
+INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('10', 'NomeDoErro10', 'Erro de compilação', 'PS2016', '3'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('11', 'NomeDoErro11', 'Interface', 'PS2016', '3'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('12', 'NomeDoErro12', 'Erro de lógica', 'PS2016', '2'); 
 INSERT INTO erro (idErro, nome, tipo, descricao, projeto_idProjeto) VALUES ('13', 'NomeDoErro13', 'Erro de lógica', 'PS2016', '1'); 
